1cea99aab2b718f0e7a8263ffc6bff1ab6593fc9
Assafkatz99 <assafkatz99@gmail.com>
2023-03-26 17:56:22 +0300

    fixed iphone interface

--- src/components/UserInterface/UserInterface Components/UpperUserCalendar/UpperUserCalendar.tsx
+++ src/components/UserInterface/UserInterface Components/UpperUserCalendar/UpperUserCalendar.tsx
@@ -21,9 +21,11 @@ const UpperUserCalendar: React.FC = () => {
     const [selectedDate, setSelectedDay] = React.useState(today);
     const [open, setOpen] = React.useState(false);
     const [month, setMonth] = React.useState<String>(dayjs().format("MMMM"));
+    const [monthNumber, setMonthNumber] = React.useState<Number>(0);
     const [year, setYear] = React.useState<number>(dayjs().year());
     const [temporaryChange, setTemporaryChange] = React.useState<[String,Number]>([month,year]);
-    const daysInMonth = dayjs(`${month} ${year}`, "MMMM YYYY").daysInMonth();
+    // const daysInMonth = dayjs(`${month} ${year}`, "MMMM YYYY").daysInMonth();
+
 
 
     useEffect(()=>{
@@ -33,14 +35,31 @@ const UpperUserCalendar: React.FC = () => {
         day_to_scroll.scrollIntoView({ behavior: 'smooth'})
       }},200)
     },[selectedDate])
+    
+    const months: Record<string, number> = {
+      January: 1,
+      February: 2,
+      March: 3,
+      April: 4,
+      May: 5,
+      June: 6,
+      July: 7,
+      August: 8,
+      September: 9,
+      October: 10,
+      November: 11,
+      December: 12,
+    };
 
-    const daysArray = [];
-    for (let i = 1; i <= daysInMonth; i++) {
-      const day = dayjs(`${month}-${i}-${year}`);
-      const dayOfWeek = day.format("dd");
-      const paddedDay = i < 10 ? `0${i}` : `${i}`;
-      daysArray.push([paddedDay, dayOfWeek]);
-    }
+const daysInMonth = new Date(year, months[`${month}`] , 0).getDate();
+const daysArray = [];
+for (let day = 1; day <= daysInMonth; day++) {
+  const date = new Date(year, months[`${month}`]-1, day);
+  const dayOfWeek = date.toLocaleString('en-US', { weekday: 'short' });
+  daysArray.push([day.toString().padStart(2, '0'), dayOfWeek.slice(0,2)]);
+}
+
+console.log(daysArray);
 
 
         const handleTodayButton =  () => {
@@ -51,13 +70,7 @@ const UpperUserCalendar: React.FC = () => {
     }
 
 
-  const months:Array<[string, number]> = [];
 
-  for (let month = 0; month < 12; month++) {
-    const monthDate = dayjs().year(year).month(month);
-    const monthName = monthDate.format("MMMM");
-    months.push([monthName,month]);
-  }
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -129,7 +142,7 @@ const UpperUserCalendar: React.FC = () => {
                   onChange={(event)=>{setTemporaryChange(prev => [event.target.value,prev[1]])}}
 
                   input={<OutlinedInput label="Month" />}>
-                    {months.map((month)=>{return <option value={month[0]}>{`${month[0]} - ` + (Number(month[1])+1)}</option>})}
+                    {Object.entries(months).map((month)=>{return <option value={month[0]}>{`${month[0]} - ` + (Number(month[1]))}</option>})}
 
 
                 </Select>
@@ -161,17 +174,11 @@ const UpperUserCalendar: React.FC = () => {
         }}>
         {daysArray.map((day)=>{
         // {[[1,"er"],[2,"er"],[3,"er"]].map((day)=>{
-        let monthInNum = "";
-        for (let i in months){
-            if (months[i][0] === month){
-                if ((months[i][1]+1) < 10 ){
-                    monthInNum=`0${months[i][1]+1}`;
-                } else{monthInNum=`${months[i][1]+1}`}
-                break;
-            }
-        }
-            return(
-                <Box id={`${monthInNum}-${day[0]}-${year}`} onClick={()=>(setSelectedDay(`${monthInNum}-${day[0]}-${year}`))} sx={{display:"flex", textAlign:"center",flexDirection:"column",alignItems:"center", justifyContent:"space-between",paddingTop:"5px",mx:"3px", paddingBottom:"15px",width:"47px", height:"100px", backgroundColor: selectedDate === `${monthInNum}-${day[0]}-${year}` ?"#4E4E61" : "#989CA9"  , borderRadius:"16px"}}>
+          let month_for_box = months[`${month}`] < 10
+          ? `0${months[`${month}`]}`
+          : `${months[`${month}`]}`;
+                    return(
+                <Box id={`${month_for_box}-${day[0]}-${year}`} onClick={()=>(setSelectedDay(`${month_for_box}-${day[0]}-${year}`))} sx={{display:"flex", textAlign:"center",flexDirection:"column",alignItems:"center", justifyContent:"space-between",paddingTop:"5px",mx:"3px", paddingBottom:"15px",width:"47px", height:"100px", backgroundColor: selectedDate === `${month_for_box}-${day[0]}-${year}` ?"#4E4E61" : "#989CA9"  , borderRadius:"16px"}}>
                     <Typography variant="h6" sx={{color:"white", fontWeight:"500",position:"relative"}}>{day[0]}<Typography variant="subtitle2" sx={{color:"white",fontSize:"11px", fontWeight:"300",position:"absolute", bottom:"-13px", left:"50%", transform:"translate(-50%)", letterSpacing:"0.5px"}}>{day[1]}</Typography></Typography>
                 </Box>
             )
